38dc3f34a73d7126160b2aac4f140095228afea8
DeepJha <deep.jhav@gmail.com>
2021-02-17 23:43:02 +0530

    v3.1.0 HOVER FUNCTIONALITY ACTIVE ON CHILD COMPONENTS

--- js/navbar.js
+++ js/navbar.js
@@ -10,3 +10,41 @@ $(document).ready(function () {
     }
   });
 });
+
+//------ TOGGLE PARENT CTAEGORY -------
+//FOR ACCESS
+$(document).ready(function () {
+  $("#parent__access").mouseenter(function () {
+    $("#child__access").stop().show();
+  });
+
+  $("#parent__access, #child__access").mouseleave(function () {
+    if (!$("#child__access").is(":hover")) {
+      $("#child__access").hide();
+    }
+  });
+});
+//FOR SECURITY
+$(document).ready(function () {
+  $("#parent__security").mouseenter(function () {
+    $("#child__security").stop().show();
+  });
+
+  $("#parent__security, #child__security").mouseleave(function () {
+    if (!$("#child__security").is(":hover")) {
+      $("#child__security").hide();
+    }
+  });
+});
+//FOR PARKING
+$(document).ready(function () {
+  $("#parent__parking").mouseenter(function () {
+    $("#child__parking").stop().show();
+  });
+
+  $("#parent__parking, #child__parking").mouseleave(function () {
+    if (!$("#child__parking").is(":hover")) {
+      $("#child__parking").hide();
+    }
+  });
+});
