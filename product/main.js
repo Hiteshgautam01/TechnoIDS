c7861b8c0180d25ad907257cba3be72169a9ccb2
Orendra Singh <orendrasingh@gmail.com>
2021-02-24 02:38:23 +0530

    added products cards

---
+++ product/main.js
@@ -0,0 +1,10 @@
+$(document).on("click", "#contact-popup-close-btn", function () {
+
+ $('#contact-popup').slideUp(800)
+
+})
+
+function openContactPopup()
+{
+    $('#contact-popup').slideDown(800);
+}
