8a4960e288f8ee831e99b00c92a4f0c58dd60bb7
Orendra Singh <orendrasingh@gmail.com>
2021-03-03 02:06:58 +0530

    revamped services section and loading animation

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,10 @@
-$(window).on('load', function() { // makes sure the whole site is loaded 
+// $(window).on('load', function() { // makes sure the whole site is loaded 
+   
+//   })
+  
+  setTimeout(function(){ 
     $('#status').fadeOut(); // will first fade out the loading animation 
     $('#preloader').delay(400).fadeOut('slow'); // will fade out the white DIV that covers the website. 
     $('body').delay(350).css({'overflow':'visible'});
-  })
-  
+
+   }, 2400);
