81c745cf038f5c59105ddcb3ffeb538b17759426
orendrasingh <orendrasingh@gmail.com>
2021-03-27 01:02:14 +0530

    Added login page

---
+++ login/main.js
@@ -0,0 +1,10 @@
+console.log("hii 1")
+var myframe=""
+$(document).ready(function() { 
+
+    $("div").click(function(){
+        alert("The paragraph was clicked.");
+      });
+    myframe=document.getElementById("myiframe")
+
+});
